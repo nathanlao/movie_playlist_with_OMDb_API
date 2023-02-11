446b377caaf9ad1372543d214e265c1edceeb6e5
nathanlao <gla93@sfu.ca>
2023-02-10 23:51:02 -0800

    Refractor the code by using async/await

--- index.js
+++ index.js
@@ -6,88 +6,84 @@ let movieWatchList = JSON.parse(localStorage.getItem("myWatchList")) || []
 
 submitForm.addEventListener("submit", searchByMovieTitle)
 
-function searchByMovieTitle(event) {
+async function searchByMovieTitle(event) {
     event.preventDefault()
 
     const movieTitle = searchInputEl.value
 
-    fetch(`http://www.omdbapi.com/?apikey=464807e&s=${movieTitle}`)
-        .then(res => res.json())
-        .then(data => {
-            // data array fetch from API
-            const dataArray = data.Search
-
-            // Store the movie id array
-            let movieIdArray = []
-            movieIdArray = dataArray.map((movie) => movie.imdbID)
-            console.log(movieIdArray)
-
-            renderMovieLists(movieIdArray)
-        })
-        .catch(error => {
-
-            movieLists.innerHTML = `
-                <section class="search-error-container">
-                    <img alt="video img" src="./images/video.png"/>
-                    <p class="search-error">
-                        Oops! Unable to find what you are looking for :D
-                    </p>
-                </section>
-            `
-        })
+    try {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=464807e&s=${movieTitle}`)
+        const data = await response.json()    
+    
+        // data array fetch from API
+        const dataArray = data.Search
+    
+        // Store the movie id array
+        let movieIdArray = []
+        movieIdArray = dataArray.map((movie) => movie.imdbID)
+    
+        renderMovieLists(movieIdArray)
+    } catch (error) {
+        movieLists.innerHTML = `
+        <section class="search-error-container">
+            <img alt="video img" src="./images/video.png"/>
+            <p class="search-error">
+                Oops! Unable to find what you are looking for :D
+            </p>
+        </section>
+    `
+    }
 }
 
 function renderMovieLists(movieIdArray) {
     movieLists.innerHTML = ""
 
     // Fetch movie according to each movie imdbID
-    movieIdArray.forEach(movieId => {
-        fetch(`http://www.omdbapi.com/?apikey=464807e&i=${movieId}`)
-            .then(res => res.json())
-            .then(data => {
+    movieIdArray.forEach(async movieId => {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=464807e&i=${movieId}`)
+        const data = await response.json()
 
-                // Check if movieId is aleady in LS
-                const inWatchList = movieWatchList.includes(data.imdbID)
-                
-                let movieHtml = ""
-                movieHtml += `
-                    <div class="movie">
-                        <img 
-                            class="movie-poster"
-                            alt="movie poster" 
-                            src="${data.Poster === "N/A" ? "./images/imageNotFound.png" : data.Poster}"
-                        />
-                        <div class="movie-content-wrapper">
-                            <header class="flex">
-                                <h4 class="movie-title">${data.Title}</h4>
-                                <div class="movie-rating-wrapper flex">
-                                    <span class="icon-star">
-                                        <i class="fa-solid fa-star"></i>
-                                    </span>
-                                    <h4 class="movie-rating">${data.imdbRating}</h4>
-                                </div>
-                            </header>
-                            <section class="movie-details flex">
-                                <p>${data.Runtime}</p>
-                                <p>${data.Genre}</p>
-                                <button 
-                                    class="add-watchlist ${inWatchList ? "disabled" : ""}"
-                                    id="add-${data.imdbID}"
-                                    onclick="addToWatchList('${data.imdbID}')"
-                                >   
-                                    ${inWatchList ?
-                                        `<i class="fa-solid fa-check"></i>` :
-                                        `<i class="fa-solid fa-circle-plus"></i>`
-                                    }
-                                    Watchlist
-                                </button>
-                            </section>
-                            <p class="movie-plot">${data.Plot}</p>
+        // Check if movieId is aleady in LS
+        const inWatchList = movieWatchList.includes(data.imdbID)
+        
+        let movieHtml = ""
+        movieHtml += `
+            <div class="movie">
+                <img 
+                    class="movie-poster"
+                    alt="movie poster" 
+                    src="${data.Poster === "N/A" ? "./images/imageNotFound.png" : data.Poster}"
+                />
+                <div class="movie-content-wrapper">
+                    <header class="flex">
+                        <h4 class="movie-title">${data.Title}</h4>
+                        <div class="movie-rating-wrapper flex">
+                            <span class="icon-star">
+                                <i class="fa-solid fa-star"></i>
+                            </span>
+                            <h4 class="movie-rating">${data.imdbRating}</h4>
                         </div>
-                    </div>
-                `
-                movieLists.innerHTML += movieHtml
-            })
+                    </header>
+                    <section class="movie-details flex">
+                        <p>${data.Runtime}</p>
+                        <p>${data.Genre}</p>
+                        <button 
+                            class="add-watchlist ${inWatchList ? "disabled" : ""}"
+                            id="add-${data.imdbID}"
+                            onclick="addToWatchList('${data.imdbID}')"
+                        >   
+                            ${inWatchList ?
+                                `<i class="fa-solid fa-check"></i>` :
+                                `<i class="fa-solid fa-circle-plus"></i>`
+                            }
+                            Watchlist
+                        </button>
+                    </section>
+                    <p class="movie-plot">${data.Plot}</p>
+                </div>
+            </div>
+        `
+        movieLists.innerHTML += movieHtml
     })
 }
 
--- watchlist.js
+++ watchlist.js
@@ -6,48 +6,45 @@ let watchlistFromLS = JSON.parse(localStorage.getItem("myWatchList")) || []
 function renderMovieLists(movieIdArray) {
     watchlist.innerHTML = ""
     // Fetch movie according to each movie imdbID
-    movieIdArray.forEach(movieId => {
-        fetch(`http://www.omdbapi.com/?apikey=464807e&i=${movieId}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
+    movieIdArray.forEach(async movieId => {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=464807e&i=${movieId}`)
+        const data = await response.json()
     
-                let movieHtml = ""
-                movieHtml += `
-                    <div class="movie">
-                        <img 
-                            class="movie-poster"
-                            alt="movie poster" 
-                            src="${data.Poster === "N/A" ? "./images/imageNotFound.png" : data.Poster}"
-                        />
-                        <div class="movie-content-wrapper">
-                            <header class="flex">
-                                <h4 class="movie-title">${data.Title}</h4>
-                                <div class="movie-rating-wrapper flex">
-                                    <span class="icon-star">
-                                        <i class="fa-solid fa-star"></i>
-                                    </span>
-                                    <h4 class="movie-rating">${data.imdbRating}</h4>
-                                </div>
-                            </header>
-                            <section class="movie-details flex">
-                                <p>${data.Runtime}</p>
-                                <p>${data.Genre}</p>
-                                <button 
-                                    class="add-watchlist"
-                                    id="add-${data.imdbID}"
-                                    onclick="removeFromWatchList('${data.imdbID}')"
-                                >
-                                    <i class="fa-solid fa-circle-minus"></i>
-                                    Remove
-                                </button>
-                            </section>
-                            <p class="movie-plot">${data.Plot}</p>
+        let movieHtml = ""
+        movieHtml += `
+            <div class="movie">
+                <img 
+                    class="movie-poster"
+                    alt="movie poster" 
+                    src="${data.Poster === "N/A" ? "./images/imageNotFound.png" : data.Poster}"
+                />
+                <div class="movie-content-wrapper">
+                    <header class="flex">
+                        <h4 class="movie-title">${data.Title}</h4>
+                        <div class="movie-rating-wrapper flex">
+                            <span class="icon-star">
+                                <i class="fa-solid fa-star"></i>
+                            </span>
+                            <h4 class="movie-rating">${data.imdbRating}</h4>
                         </div>
-                    </div>
-                `
-                watchlist.innerHTML += movieHtml
-            })
+                    </header>
+                    <section class="movie-details flex">
+                        <p>${data.Runtime}</p>
+                        <p>${data.Genre}</p>
+                        <button 
+                            class="add-watchlist"
+                            id="add-${data.imdbID}"
+                            onclick="removeFromWatchList('${data.imdbID}')"
+                        >
+                            <i class="fa-solid fa-circle-minus"></i>
+                            Remove
+                        </button>
+                    </section>
+                    <p class="movie-plot">${data.Plot}</p>
+                </div>
+            </div>
+        `
+        watchlist.innerHTML += movieHtml
     })
 }
 
